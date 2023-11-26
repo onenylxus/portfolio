05fcd026f045cd588e50ba23d6d3d78e2dd86c81
Nicholas Ng <onenylxus@gmail.com>
2023-11-26 23:10:31 +0800

    About section

--- main.js
+++ main.js
@@ -3,6 +3,9 @@ const navMenu = document.querySelector('.nav-menu');
 const navToggle = document.querySelector('.nav-toggle');
 const navClose = document.querySelector('.nav-close');
 const navLink = document.querySelectorAll('.nav-link');
+const contributions = document.querySelector('.contributions');
+const publicProjects = document.querySelector('.public-projects');
+const followers = document.querySelector('.followers');
 
 // Show menu after toggle button clicked
 if (navToggle) {
@@ -24,3 +27,22 @@ navLink.forEach(element => {
     navMenu.classList.remove('show-menu');
   });
 });
+
+// Fill in dynamic data in about section
+(async () => {
+  // Format number function
+  const formatNumber = (value) => {
+    value = +value;
+    let p = 10 ** (Math.floor(Math.log10(value)) - 1);
+    return value < 10 ? `${value}` : `${Math.floor(Math.floor(value / p) / 5) * 5 * p}+`;
+  };
+
+  // Get data from API sources
+  const data1 = await (await fetch('https://github-contributions-api.deno.dev/onenylxus.json')).json();
+  const data2 = await (await fetch('https://api.github.com/users/onenylxus')).json();
+
+  // Apply values to about information
+  contributions.innerHTML = formatNumber(data1.totalContributions);
+  publicProjects.innerHTML = formatNumber(data2.public_repos);
+  followers.innerHTML = formatNumber(data2.followers);
+})();
