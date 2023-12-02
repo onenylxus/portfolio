c41e5fcc6c6a6dd51d038d3746463584dc773f2e
Nicholas Ng <onenylxus@gmail.com>
2023-12-02 23:23:10 +0800

    Projects section

--- main.js
+++ main.js
@@ -58,3 +58,19 @@ skillsHeader.forEach((header) => header.addEventListener('click', () => {
     header.parentNode.className = 'skills-content skills-open';
   }
 }));
+
+// Set up swiper
+const swiper = new Swiper('.projects-container', {
+  cssMode: true,
+  loop: true,
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+  mousewheel: true,
+  keyboard: true,
+});
